18487430da443eebde704a27c0a4482b2e314a44
MuhammadAnasRazavi <muhammadanasrazavi@gmail.com>
2024-03-31 20:50:58 +0530

    Added lemonade process logic.

--- app/src/main/java/com/example/digitallemonadeapp/MainActivity.kt
+++ app/src/main/java/com/example/digitallemonadeapp/MainActivity.kt
@@ -5,6 +5,8 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -18,6 +20,11 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -49,6 +56,44 @@ class MainActivity : ComponentActivity() {
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun DigitalLemonadeApp(modifier: Modifier = Modifier) {
+
+    var squeezeCount by remember {
+        mutableIntStateOf(0)
+    }
+
+    var result by remember {
+        mutableIntStateOf(1)
+    }
+
+    var squeezeTime by remember {
+        mutableIntStateOf((2..4).random())
+    }
+
+    val imageResource = when (result) {
+        1 -> R.drawable.lemon_tree
+        2 -> R.drawable.lemon_squeeze
+        3 -> R.drawable.lemon_drink
+        else -> R.drawable.lemon_restart
+    }
+
+    val instructionResource = when (result) {
+        1 -> R.string.lemon_tree_instruction
+        2 -> R.string.lemon_squeeze_instruction
+        3 -> R.string.lemonade_drink_instruction
+        else -> R.string.empty_glass_instruction
+    }
+
+    val descriptionResource = when (result) {
+        1 -> R.string.lemon_tree_text
+        2 -> R.string.lemon_text
+        3 -> R.string.lemonade_text
+        else -> R.string.empty_glass_text
+    }
+
+    var firstTime by remember {
+        mutableStateOf(true)
+    }
+
     Column(
         modifier = modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally
     ) {
@@ -63,21 +108,45 @@ fun DigitalLemonadeApp(modifier: Modifier = Modifier) {
         )
         Spacer(modifier = Modifier.height(125.dp))
         Image(
-            painter = painterResource(id = R.drawable.lemon_tree),
-            contentDescription = stringResource(id = R.string.lemon_tree_text),
+            painter = painterResource(id = imageResource),
+            contentDescription = stringResource(id = descriptionResource),
             modifier = Modifier
                 .height(350.dp)
                 .width(300.dp)
+                .border(
+                    width = 2.dp,
+                    shape = RoundedCornerShape(
+                        topStart = 50.dp, topEnd = 25.dp, bottomStart = 25.dp, bottomEnd = 50.dp
+                    ),
+                    color = Color(105, 205, 216)
+                )
                 .clip(
                     RoundedCornerShape(
-                        topStart = 75.dp, topEnd = 30.dp, bottomStart = 30.dp, bottomEnd = 75.dp
+                        topStart = 50.dp, topEnd = 25.dp, bottomStart = 25.dp, bottomEnd = 50.dp
                     )
                 )
                 .background(Color(0xFFc4ecd2))
+                .clickable {
+                    result = when (result) {
+                        1, 3 -> result + 1
+                        2 -> {
+                            squeezeCount += 1
+                            if (squeezeCount != squeezeTime) {
+                                2
+                            } else {
+                                squeezeCount = 0
+                                squeezeTime = (2..4).random()
+                                3
+                            }
+                        }
+
+                        else -> 1
+                    }
+                }
         )
-        Spacer(modifier = Modifier.height(16.dp))
+        Spacer(modifier = Modifier.height(20.dp))
         Text(
-            text = stringResource(id = R.string.lemon_tree_instruction), fontSize = 18.sp
+            text = stringResource(id = instructionResource), fontSize = 18.sp
         )
     }
 }
